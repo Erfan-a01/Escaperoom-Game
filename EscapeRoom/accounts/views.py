6bbef89b38911583c5228cfcbab13ce00dd20465
Erfan-AB01 <erfan.abuee@gmail.com>
2023-08-17 13:13:35 +0330

    phase1 completed(login and logout page)

--- EscapeRoom/accounts/views.py
+++ EscapeRoom/accounts/views.py
@@ -1,7 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+def user_register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            user.first_name = cd['first_name']
+            user.last_name = cd['last_name']
+            user.save()
+            messages.success(request, 'User registered successfully')
+            return redirect('home_page')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
 
 
-def say_hello(request):
-    return render(request, 'hello.html')
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Loged in successfully', 'success')
+                return redirect('home_page')
+            else:
+                messages.error(request, 'Username or Password is wrong!!', extra_tags='danger')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'logged out successfully!')
+    return redirect('home_page')
+
+
+def home_page(request):
+    return render(request, 'home.html')
